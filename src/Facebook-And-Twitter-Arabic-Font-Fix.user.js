8da5e05728cc1b29c0ba46c3bdf3c989e4124ab9
Xeriab Nabil <kodeburner@gmail.com>
2016-06-21 02:42:25 +0300

    Merged the scripts in one file

---
+++ src/Facebook-And-Twitter-Arabic-Font-Fix.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name        Facebook & Twitter Arabic Font Fix
+// @namespace   Facebook/Twitter
+// @description Fix Facebook/Twitter font for Arab users
+// @author      Xeriab Nabil (aka KodeBurner) <kodeburner@gmail.com>
+// @encoding    utf-8
+// @license     MIT
+// @icon        https://raw.github.com/xeriab/userscripts/master/artwork/fatar.png
+// @homepage    https://xeriab.github.io/projects/userscripts/
+// @twitterURL  https://twitter.com/xeriab
+// @facebookURL https://facebook.com/xeriab
+// @include     http://facebook.com/*
+// @include     https://facebook.com/*
+// @include     http://*.facebook.com/*
+// @include     https://*.facebook.com/*
+// @include     http://twitter.com/*
+// @include     https://twitter.com/*
+// @include     http://*.twitter.com/*
+// @include     https://*.twitter.com/*
+// @include     javascript:*
+// @run-at      document-start
+// ==/UserScript==
+
+/*jslint browser: true, vars: true */
+/*jshint unused: true, strict: true, nonew: true */
+/*globals GM_addStyle, PRO_addStyle, addStyle */
+
+(function () {
+    'use strict';
+
+    var style = [
+        '/* Applies font to all generic HTML tags */',
+        '',
+        '* {',
+        '',
+        "    font-family: 'Segoe UI', 'Noto Naskh Arabic', 'Tahoma', sans !important;",
+        '',
+        '}'
+    ].join('\n');
+
+    if (typeof GM_addStyle !== 'undefined') {
+        GM_addStyle(style);
+    } else if (typeof PRO_addStyle !== 'undefined') {
+        PRO_addStyle(style);
+    } else if (typeof addStyle !== 'undefined') {
+        addStyle(style);
+    } else {
+        var node = document.createElement('style');
+        var docHeads = document.getElementsByTagName('head');
+
+        node.type = 'text/css';
+        node.appendChild(document.createTextNode(style));
+
+        if (docHeads.length > 0) {
+            docHeads[0].appendChild(node);
+        } else {
+            // No head yet, stick it whereever
+            document.documentElement.appendChild(node);
+        }
+    }
+
+}());
